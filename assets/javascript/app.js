2101cc437a9b14d418d9a8747a322c566e3be5d8
Chaelor <joshsblizzard@gmail.com>
2018-07-02 13:38:36 -0500

    Everything is working except for reset, and 1 second delay

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,34 +55,50 @@ $(document).ready(function () {
     //Store all of the questions into an array so I can access them later
     quizQuestionsArray = [];
     quizQuestionsArray.push(quizQuestionOne, quizQuestionTwo, quizQuestionThree, quizQuestionFour, quizQuestionFive);
+
+    //See above but with answers.
     correctAnswersArray = [];
     correctAnswersArray.push(quizQuestionOne.correctAnswer, quizQuestionTwo.correctAnswer, quizQuestionThree.correctAnswer, quizQuestionFour.correctAnswer, quizQuestionFive.correctAnswer, )
-    console.log(correctAnswersArray);
 
-    //The only timer, go back and change this to have a few timers when I refresh the page
-
+    //jQuery Selections
     var quizArea = $(".quizArea");
     var welcome = $(".welcome");
+
+    //Variables
+    //Count down from ten
     var quizTimer = 10;
+
+    //Counter for questions
     var counter = 0;
+
+    //Display the question # correctly
     var questionNumber = 1;
+
+    //User choice goes here
     var userChoice = 0;
+
+    //Score keeping
     var incorrect = 0;
     var correct = 0;
 
+    //Makes a button then lets it be clickable to initialize game
     function startGame() {
-        quizArea.html("<p><button class='btn-style btnStart'> Click to begin </button></p>");
+        quizArea.html("<button class='btn-style btnStart'> Click to begin </button>");
     }
     startGame();
 
-
+    //Render the game after the button was clicked
     $(".btnStart").on("click", function () {
         quizRender();
-    })
+    });
 
-
+    //Countdown timer
     function countdownTimer() {
+
+        //Reset the timer here if need be, fun fact: You will.
         timer = setInterval(decrease, 1000);
+
+        //Function for the interval to do work add classes to change color. Constantly count down
         function decrease() {
             if (quizTimer > 0) {
                 quizTimer--;
@@ -98,34 +114,48 @@ $(document).ready(function () {
                 $(".welcome").removeClass("yellowTimer");
                 $(".welcome").addClass("redTimer");
             }
-            if(quizTimer === 0) {
+            //If the user didn't answer we'll do this instead
+            if (quizTimer === 0) {
                 renderNoTimeRemaining();
                 clearInterval(timer);
             }
+
+            //Display time remaining at the top
             welcome.html("<div><h2> Time remaining: " + quizTimer + "</h2></div>")
         }
     }
 
+    //Render the game, give the divs classes to be clickable
     function quizRender() {
+
+        //Start the timer
         countdownTimer();
+
+        //Changes the html to display the question
         quizArea.html("<div class='setup'> Question " + questionNumber + ": </div>" +
-            "<div class='question'> " + quizQuestionsArray[counter].question + "</div>")
+            "<div class='question'> " + quizQuestionsArray[counter].question + "</div>");
+
+        //Appends the answers for questions to the above html
         quizArea.append(
             "<div class='answer'>" + quizQuestionsArray[counter].answers.a + "</div>" +
             "<div class='answer'>" + quizQuestionsArray[counter].answers.b + "</div>" +
             "<div class='answer'>" + quizQuestionsArray[counter].answers.c + "</div>");
+
+        //Adds to the counter displaying the question number NOT TO THE QUESTION COUNTER!!!!!!!
         questionNumber++;
     }
 
+    //If the userchoice = correctAnswer do this. Removes all colored classes, tells user they were right.
     function renderWin() {
         welcome.removeClass("greenTimer");
         welcome.removeClass("yellowTimer");
         welcome.removeClass("redTimer");
-        quizArea.html("<div><h2>Congratulations! You got the question correct!</h2></div>");
+        quizArea.html("<div class='greenTimer'><h2>Congratulations! You got the question correct!</h2></div>");
         correct++;
         setTimeout(transitionTimer, 5000);
     }
 
+    //If the user was wrong, do this:
     function renderLoss() {
         welcome.removeClass("greenTimer");
         welcome.removeClass("yellowTimer");
@@ -135,6 +165,7 @@ $(document).ready(function () {
         setTimeout(transitionTimer, 5000);
     }
 
+    //If the user didn't select anything, do this:
     function renderNoTimeRemaining() {
         welcome.removeClass("greenTimer");
         welcome.removeClass("yellowTimer");
@@ -145,28 +176,21 @@ $(document).ready(function () {
         setTimeout(transitionTimer, 5000);
     }
 
+    //Wait 5 seconds between each question and do this
     function transitionTimer() {
         if (questionNumber < 6) {
             quizRender();
             quizTimer = 10;
-            countdownTransitionTimer();
+
+        //Display the results of the game
         } else {
             welcome.html("<h1> Thanks for playing!</h1>")
             quizArea.html("<div> You got: " + incorrect + "/5 questions incorrect</div>");
             quizArea.append("<div> You got: " + correct + "/5 questions correct</div>");
-            quizArea.append("<div><button class='btn-style btnReset'> Try again! </button></div>");
+
+            //At the end of the game, display this button to reset the game.
+            quizArea.append("<button class='btn-style btnReset'> Click to reset! </button>");
         }
-    }
-
-    function resetGame() {
-    quizTimer = 10;
-    counter = 0;
-    questionNumber = 1;
-    userChoice = 0;
-    incorrect = 0;
-    correct = 0;
-
-    quizRender();
     }
 
     //User selects their choice by clicking on some class with .answer
@@ -181,7 +205,7 @@ $(document).ready(function () {
             renderWin();
             counter++;
 
-        //If they got it wrong
+            //If they got it wrong
         } else {
             clearInterval(timer);
             renderLoss();
@@ -189,8 +213,21 @@ $(document).ready(function () {
         }
     });
 
-    $(".btnReset").on("click", function() {
+    //Reset the game IF THIS WAS WORKING HRNGGGGG
+    function resetGame() {
+        quizTimer = 10;
+        counter = 0;
+        questionNumber = 1;
+        userChoice = 0;
+        incorrect = 0;
+        correct = 0;
+
+        quizRender();
+    }
+
+    //THIS DOESN'T WORK EITHER HRNGGGGG
+    $(".btnReset").on("click", function () {
+        console.log("Working!");
         resetGame();
     });
-
 });
