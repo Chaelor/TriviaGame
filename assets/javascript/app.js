d742e7b4f41f97a2c97e112904d166f18916560a
Chaelor <joshsblizzard@gmail.com>
2018-07-02 09:59:34 -0500

    upadting the .app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
                 b: "Mt. Saint Helens",
                 c: "Mt. Everest"
             },
-            correctAnswer: "c"
+            correctAnswer: "Mt. Everest"
         };
 
     quizQuestionTwo =
@@ -18,7 +18,7 @@ $(document).ready(function () {
                 b: "The Atomic Bomb",
                 c: "The city of New York"
             },
-            correctAnswer: "b"
+            correctAnswer: "The Aromic Bomb"
         }
     quizQuestionThree =
         {
@@ -28,7 +28,7 @@ $(document).ready(function () {
                 b: "a^2 + b^2 = c^2",
                 c: "1/2 bh"
             },
-            correctAnswer: "a"
+            correctAnswer: "y = mx + b"
         }
     quizQuestionFour =
         {
@@ -38,7 +38,7 @@ $(document).ready(function () {
                 b: "RaspberryPI",
                 c: "Ardunio"
             },
-            correctAnswer: "a",
+            correctAnswer: "Mint",
         }
     quizQuestionFive =
         {
@@ -48,20 +48,87 @@ $(document).ready(function () {
                 b: "The Fairly Odd Parents",
                 c: "Full House"
             },
-            correctAnswer: "b"
+            correctAnswer: "The Fairly Odd Parents"
         }
 
-//Store all of the questions into an array so I can access them later
-quizQuestionArray=[];
-quizQuestionArray.push(quizQuestionOne, quizQuestionTwo, quizQuestionThree, quizQuestionFour, quizQuestionFive);
-questionAnswers=[];
-console.log(quizQuestionArray);
-for(let i = 0; i < quizQuestionArray.length; i++){
-    console.log(quizQuestionArray[i].question);
-    console.log(quizQuestionArray[i].answers.a);
-    console.log(quizQuestionArray[i].answers.b);
-    console.log(quizQuestionArray[i].answers.c);
-}
 
-quizQuestionOne.answers.each(function() {$(".quizContent").html("<input type='radio'>")});
+    //Store all of the questions into an array so I can access them later
+    quizQuestionsArray = [];
+    quizQuestionsArray.push(quizQuestionOne, quizQuestionTwo, quizQuestionThree, quizQuestionFour, quizQuestionFive);
+    correctAnswersArray = [];
+    correctAnswersArray.push(quizQuestionOne.correctAnswer, quizQuestionTwo.correctAnswer, quizQuestionThree.correctAnswer, quizQuestionFour.correctAnswer, quizQuestionFive.correctAnswer, )
+    console.log(correctAnswersArray);
+
+    //The only timer, go back and change this to have a few timers when I refresh the page
+
+    var quizArea = $(".quizArea");
+    var quizTimer = 10;
+    var counter = 0;
+    let questionNumber = 1;
+    var selectedAnswer =  0;
+    var incorrect = 0;
+    var correct = 0;
+
+    function startGame() {
+        quizArea.html("<p><button class='btn-style btn'> Click to begin </button></p>");
+    }
+    startGame();
+
+
+    $(".btn").on("click", function () {
+        countdownTimer();
+        quizRender();
+    })
+
+    function countdownTimer() {
+        setInterval(decrease, 1000);
+        function decrease() {
+            if (quizTimer > 0) {
+                quizTimer--;
+            }
+            if (quizTimer > 6) {
+                $(".welcome").addClass("greenTimer");
+            }
+            if (quizTimer < 6 && quizTimer > 3) {
+                $(".welcome").removeClass("greenTimer");
+                $(".welcome").addClass("yellowTimer");
+            }
+            if (quizTimer < 3) {
+                $(".welcome").removeClass("yellowTimer");
+                $(".welcome").addClass("redTimer");
+            }
+            $(".welcome").html("<div> Time remaining: " + quizTimer + "</div>")
+        }
+    }
+
+    function quizRender() {
+        //Add a go to results page
+        if (questionNumber === 6) {
+            $(".welcome").html("<h1> Thanks for playing!</h1>")
+            quizArea.html("<p> You got: " + incorrect + "/5 questions incorrect</p>");
+            quizArea.html("<p> You got: " + correct + "/5 questions correct</p>")
+        } else {
+            quizArea.html("<div class='setup'> Question " + questionNumber + ": </div>" +
+                "<div class='question'> " + quizQuestionsArray[counter].question + "</div>")
+            quizArea.append(
+                "<p class='answer'> a: " + quizQuestionsArray[counter].answers.a + "</p>" +
+                "<p class='answer'> b: " + quizQuestionsArray[counter].answers.b + "</p>" +
+                "<p class='answer'> c: " + quizQuestionsArray[counter].answers.c + "</p>");
+            questionNumber++;
+        }
+    }
+
+    //Change this to incorporate a victory//loss screen
+    $("body").on("click", ".answer", function (event) {
+        selectedAnswer = $(this).text();
+        if (selectedAnswer === correctAnswersArray[counter]) {
+            console.log(this + "Win");
+            console.log("Win");
+            correct++
+        } else {
+            console.log(this + "Lose");
+            console.log("Lose");
+            incorrect++;
+        }
+    })
 });
